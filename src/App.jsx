e97db19da887b4a5a0d0875b658b529751747748
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2024-05-25 16:45:58 +0200

    come CSS improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,3 @@
-/* import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './Components/Layout/Home';
-import SpecificVenue from './Components/Venues/SpecificVenue';
-import ContactPage from './Components/Contact/ContactPage';
-import AboutPage from './Components/About/About';
-import NewVenueForm from './Components/Venues/NewVenueForm';
-import Layout from './Components/Layout/Layout';
-import RegisterPage from './Components/User/Register/RegisterPage';
-import LoginPage from './Components/User/Login/LoginPage';
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Layout />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/venue/:id' element={<SpecificVenue />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/newvenue' element={<NewVenueForm />} />
-        <Route path='*' element={<div>Route not found</div>} />
-      </Routes>
-
-    </BrowserRouter>
-  );
-}
-
-export default App; */
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Layout/Home';
@@ -40,22 +8,25 @@ import NewVenueForm from './Components/Venues/NewVenueForm';
 import Layout from './Components/Layout/Layout';
 import RegisterPage from './Components/User/Register/RegisterPage';
 import LoginPage from './Components/User/Login/LoginPage';
+import '../src/Components/Layout/App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/venue/:id' element={<SpecificVenue />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/newvenue' element={<NewVenueForm />} />
-        <Route path='*' element={<div>Route not found</div>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Layout />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/venue/:id' element={<SpecificVenue />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/newvenue' element={<NewVenueForm />} />
+          <Route path='*' element={<div>Route not found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
