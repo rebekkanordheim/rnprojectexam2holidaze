2fef6a707eae3bb390dc9a2574c8273eff1ec7e7
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2024-12-07 14:51:29 +0100

    added JSDocs to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,14 @@ import RegisterPage from "./Components/User/Register/RegisterPage";
 import LoginPage from "./Components/User/Login/LoginPage";
 import Profile from "./Components/User/Profile/Profile";
 
+/**
+ * Main application component that handles routing and layout.
+ *
+ * This component sets up the routes for the application and renders the corresponding components
+ * based on the URL path. It also includes a layout component that is displayed on all pages.
+ *
+ * @returns {JSX.Element} The main App component that renders the routing setup.
+ */
 function App() {
   return (
     <BrowserRouter>
