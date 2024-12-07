9da2f47a96361375c0e28508bb1533367dbce689
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2024-12-07 14:41:17 +0100

    added JSDocs to authUtils file

--- src/Components/User/authUtils.js
+++ src/Components/User/authUtils.js
@@ -1,7 +1,7 @@
 /**
  * Checks if a user is authenticated based on the presence of a JWT token and an API key in the localStorage.
  *
- * @returns {boolean} Returns true if a user is authenticated, otherwise false.
+ * @returns {boolean} Returns `true` if both the JWT token and API key are present in localStorage, indicating the user is authenticated; otherwise, returns `false`.
  */
 export function isAuthenticated() {
   const jwtToken = localStorage.getItem("jwtToken");
